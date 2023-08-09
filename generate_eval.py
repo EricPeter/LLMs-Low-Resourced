3464c2cc29ea4791b93b2d7cfc8983a13cf73e13
EricPeter <kigaye.ericpeter@gmail.com>
2023-08-09 13:03:40 +0000

    made changes to generate eval

--- generate_eval.py
+++ generate_eval.py
@@ -5,9 +5,9 @@ import gradio as gr
 import json
 import pandas as pd
 
-assert (
-    "LlamaTokenizer" in transformers._import_structure["models.llama"]
-), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
+#assert (
+#    "LlamaTokenizer" in transformers._import_structure["models.llama"]
+#), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
 from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
 
 tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf")
